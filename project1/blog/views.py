32e109a07694aaa3d56714310e93ea432ef0f8c2
saurabh <saurabhthorat16@gmail.com>
2024-09-27 15:09:25 +0530

    home4.vies.py

--- project1/blog/views.py
+++ project1/blog/views.py
@@ -84,3 +84,9 @@ def home3(request):
 #     posts1 = Post.objects.all().order_by('-created_at')
 #
 #     return render(request, 'blog/home.html', {'posts1': posts1})
+
+#saurabh
+def home4(request):
+    posts1 = Post.objects.all().order_by('-created_at')
+
+    return render(request, 'blog/home.html', {'posts1': posts1})
